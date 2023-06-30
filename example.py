cdffa042d70a84535305721fba49bb144d37f4af
rubences <91731294+rubences@users.noreply.github.com>
2023-06-30 17:24:03 +0000

    cambios

--- example.py
+++ example.py
@@ -1,19 +1,20 @@
-#ldap_example.py
+# ldap_example.py
 from ldap3 import Server, Connection, ALL
 from ldap3.core.exceptions import LDAPException
 
+
 def connect_to_ldap_server(host, port, user, password):
-    
+
     try:
         # Crear una instancia del objeto `Server`
         s = Server(host, port=port, get_info=ALL)
 
         # Crear una instancia del objeto `Connection`
-        c = Connection(s, 
-                       auto_bind=True, 
-                       user=user, 
+        c = Connection(s,
+                       auto_bind=True,
+                       user=user,
                        password=password)
-        
+
         # Imprimir información sobre el servidor LDAP
         print(s.info)
 
@@ -21,10 +22,9 @@ def connect_to_ldap_server(host, port, user, password):
         print(f"Error while connecting to LDAP server: {e}")
         return None
 
-# Actualizar estos valores con los valores correctos para tu servidor y credenciales LDAP
-host = 'localhost'
-port = 389
-user = 'cn=admin,dc=mydomain,dc=com'
-password = 'password'
 
-connect_to_ldap_server(host, port, user, password)
+def main():
+    # Actualizar estos valores con los valores correctos para tu servidor y credenciales LDAP
+    host = 'localhost'
+    port = 389
+    user
